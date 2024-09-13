d533ac96de7bbe48ec33b10e04e08f98d6d6a63b
cao quang huu <caoquanghuu@gmail.com>
2024-09-13 11:43:57 +0700

    fix: game size

--- src/GameScene/Constants.ts
+++ src/GameScene/Constants.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 export namespace AppConstants {
-    export const appWidth: number = 960;
-    export const appHeight: number = 640;
+    export const appWidth: number = 1200;
+    export const appHeight: number = 800;
     export const appColor: string = '#1099bb';
 }
