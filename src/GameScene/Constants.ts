7d9b9b8c36c986c6e94bd111a2e949e6a161cc74
cao quang huu <caoquanghuu@gmail.com>
2024-09-16 14:44:00 +0700

    add: constant for objects property

--- src/GameScene/Constants.ts
+++ src/GameScene/Constants.ts
@@ -1,3 +1,5 @@
+import { TowerType } from 'src/Type';
+
 /* eslint-disable @typescript-eslint/no-namespace */
 export namespace AppConstants {
     export const appWidth: number = 960;
@@ -18,5 +20,47 @@ export namespace AppConstants {
     export const SpinSize = {
         width: appWidth - mapSize.width,
         height: appHeight - UISize.height
+    };
+
+    export const textureName = {
+        tower: {
+            crystalMaiden: 'crystal-maiden',
+            mirana: 'mirana',
+            tinker: 'tiner'
+        }
+    };
+
+    export const bulletCount = 5;
+    export const towerCount = 5;
+    export const enemiesCount = 5;
+
+    export const dame = {
+        CM: 0,
+        Mirana: 1,
+        Tinker: 2
+    };
+
+    export const goldCost = {
+        CM: 50,
+        Mirana: 70,
+        Tinker: 100
+    };
+
+    export const effectArena = {
+        CM: 200,
+        Mirana: 200,
+        Tinker: 200
+    };
+
+    export const enemiesHp = {
+        tank1: 5,
+        tank2: 10,
+        tank3: 15
+    };
+
+    export const enemiesDameDeal = {
+        tank1: 1,
+        tank2: 2,
+        tank3: 3
     };
 }
