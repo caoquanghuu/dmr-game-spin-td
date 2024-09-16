f95496e6f5d8d894d979179341592ad4dcc9bb44
cao quang huu <caoquanghuu@gmail.com>
2024-09-16 14:44:14 +0700

    add: base engine

---
+++ src/MoveEngine/BaseEngine.ts
@@ -0,0 +1,24 @@
+import { Direction } from 'src/Type';
+
+export class BaseEngine {
+    private _direction: Direction | number;
+
+    constructor(isMoveByEnumDirection: boolean) {
+        if (isMoveByEnumDirection) {
+            this._direction = Direction.STAND;
+        } else {
+            this._direction = 0;
+        }
+    }
+
+    get direction(): Direction | number {
+        return this._direction;
+    }
+
+    set direction(direction: Direction | number) {
+        this._direction = direction;
+    }
+    public update(dt: number) {
+
+    }
+}
