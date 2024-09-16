b555997876a625d3e653264d3df101fba74a77ad
cao quang huu <caoquanghuu@gmail.com>
2024-09-16 14:44:32 +0700

    add: bullet class

---
+++ src/ObjectsPool/Bullet.ts
@@ -0,0 +1,32 @@
+import { BulletType } from 'src/Type';
+import { BaseObject } from './BaseObject';
+import { BaseEngine } from 'src/MoveEngine/BaseEngine';
+
+export class Bullet extends BaseObject {
+    private _dame: number;
+    private _bulletType: BulletType;
+    constructor(bulletType: BulletType) {
+        super(bulletType);
+        this._bulletType = bulletType;
+        this.moveEngine = new BaseEngine(false);
+    }
+
+    get dame(): number {
+        return this._dame;
+    }
+
+    set dame(dame: number) {
+        this._dame = dame;
+    }
+
+    get bulletType(): BulletType {
+        return this._bulletType;
+    }
+    private _changeDirectionOnMove() {
+        this.image.angle = this.moveEngine.direction;
+    }
+    public update(dt: number): void {
+        this.move(dt);
+        this._changeDirectionOnMove();
+    }
+}
