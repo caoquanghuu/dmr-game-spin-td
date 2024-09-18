e5b27a7ce340b74d6a8e3783906e2cfa9a89cde7
cao quang huu <caoquanghuu@gmail.com>
2024-09-18 11:58:00 +0700

    fix: direction of bullet change when target move

--- src/ObjectsPool/Bullet.ts
+++ src/ObjectsPool/Bullet.ts
@@ -1,10 +1,14 @@
 import { BulletType } from '../Type';
 import { BaseObject } from '../ObjectsPool/BaseObject';
 import { BaseEngine } from '../MoveEngine/BaseEngine';
+import { PointData } from 'pixi.js';
+import { calculateAngleOfVector } from '../Util';
 
 export class Bullet extends BaseObject {
     private _dame: number;
     private _bulletType: BulletType;
+    private _target: PointData;
+    private _isMoving: boolean = false;
     constructor(bulletType: BulletType) {
         super(bulletType);
         this._bulletType = bulletType;
@@ -22,11 +26,16 @@ export class Bullet extends BaseObject {
     get bulletType(): BulletType {
         return this._bulletType;
     }
-    private _changeDirectionOnMove() {
-        this.image.angle = this.moveEngine.direction;
+
+    private _updateDirection() {
+        const newDirection = calculateAngleOfVector(this.position, this._target);
+        this.image.angle = newDirection;
+        this.direction = newDirection;
     }
+
     public update(dt: number): void {
+        if (!this._isMoving) return;
+        this._updateDirection();
         this.move(dt);
-        this._changeDirectionOnMove();
     }
 }
--- src/Util.ts
+++ src/Util.ts
@@ -1,1 +1,11 @@
+import { PointData } from 'pixi.js';
+
 export const switchFn = (lookupObject, defaultCase = '_default') => expression => (lookupObject[expression] || lookupObject[defaultCase])();
+
+export function calculateAngleOfVector(p1: PointData, p2: PointData): number {
+    const dx = p2.x - p1.x;
+    const dy = p2.y - p1.y;
+    const radian = Math.atan2(dy, dx);
+    const angle = radian * (180 / Math.PI);
+    return angle;
+}
