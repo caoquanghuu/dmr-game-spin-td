c8e68ba269862cfb3922d4d13bc53ae561108075
cao quang huu <caoquanghuu@gmail.com>
2024-10-03 14:20:47 +0700

    update: comment for methods

--- src/Controller/TowerController.ts
+++ src/Controller/TowerController.ts
@@ -22,21 +22,39 @@ export class TowerController {
         return this._towers;
     }
 
+    /**
+     * method create tower be called when ui success buy tower
+     * @param option  type of tower and the base where player chose to build tower
+     * @returns return undefine when that position not available
+     */
     public _createTower(option: {towerType: TowerType, baseTower: Sprite}): void {
+        // get tower from pool
         const tower = this._getTowerFromPool(option.towerType);
+
+        // create a variable info to check that position with tower size available or not
         const info = { position: [{ x: option.baseTower.position.x, y: option.baseTower.position.y }], buildingSize: tower.buildingSize };
+        // in case cant not build then return
         if (!this._checkBuildingSpace(info)) {
-            console.log('cant not build');
+            // return tower to pool
             this._returnTowerToPool(tower);
             return;
         }
+
+        // in case that position is available
+
+        // update matrix map and get position of base towers tower need
         const towerBasesPosition = this._changeMatrixMap(true, info);
+        // get bases from map and assign to tower
         const towerBases = this._getTowerBases(towerBasesPosition);
+        tower.baseTower = towerBases;
+
+        // set position for images of tower
         tower.position = { x: option.baseTower.x, y: option.baseTower.y - 25 };
         tower.circleImage.position = { x: tower.position.x + tower.image.width / 2, y: tower.image.position.y + tower.image.height / 2 };
         tower.circleImage.zIndex = AppConstants.zIndex.tower;
         tower.image.zIndex = tower.position.y;
-        tower.baseTower = towerBases;
+
+        // set new event for bases that when click will render tower information
         tower.baseTower.forEach(base => {
             base.removeAllListeners();
             base.on('pointerdown', () => {
@@ -67,14 +85,20 @@ export class TowerController {
         sound.play(AppConstants.soundName.mainSound, { sprite:AppConstants.soundName.buildingCompleted });
     }
 
+    /**
+     * method remove tower
+     * @param towerId  id of tower will be remove
+     * @returns return undefine when cant find tower
+     */
     private _removeTower(towerId: number): void {
         const i = this._towers.findIndex(tower => tower.id === towerId);
         if (i === -1) {
-            console.log(`tower have id ${towerId} not found`);
             return;
         }
 
         const tower = this._towers[i];
+
+        // reset event for bases
         tower.baseTower.forEach (base => {
             base.removeAllListeners();
             base.on('pointerdown', () => {
@@ -82,8 +106,12 @@ export class TowerController {
                 sound.play(AppConstants.soundName.mainSound, { sprite: AppConstants.soundName.selectedBuilding });
             });
         });
+
+        // update matrix map to available build tower
         const basesPosition = tower.baseTower.map(base => base.position);
         this._changeMatrixMap(false, { position: basesPosition, buildingSize: tower.buildingSize });
+
+        // reset tower property to default
         tower.reset();
         this._returnTowerToPool(tower);
         Emitter.emit(AppConstants.event.removeChildFromScene, tower.image);
@@ -142,6 +170,12 @@ export class TowerController {
     }
 
 
+    /**
+     * method change matrix map and return position available when build tower
+     * @param isBuilding building or destroy
+     * @param info position and building size
+     * @returns
+     */
     private _changeMatrixMap(isBuilding: boolean, info: {position: PointData[], buildingSize: PointData}): PointData[] | undefined {
         const matrixValue = isBuilding ? AppConstants.matrixMapValue.tower : AppConstants.matrixMapValue.availableTowerBuild;
         const matrixPoint: PointData = { x: info.position[0].x / AppConstants.matrixSize, y: info.position[0].y / AppConstants.matrixSize };
