89faaf21c638deb4add61ccb7faa05696f57bc12
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2024-10-09 23:38:03 +0700

    fix: move check collision and find correct position before collison function to utils file

--- src/Util.ts
+++ src/Util.ts
@@ -22,6 +22,12 @@ const Emitter = {
 Object.freeze(Emitter);
 export default Emitter;
 
+ /**
+     * method check collision between 2 circle base on calculate distance
+     * @param c1
+     * @param c2
+     * @returns return result as boolean have collision or not
+     */
 export function isCollision(c1: Circle, c2: Circle): boolean {
     const r = c1.radius + c2.radius;
     const distance = Math.sqrt((c1.position.x - c2.position.x) * (c1.position.x - c2.position.x) + (c1.position.y - c2.position.y) * ((c1.position.y - c2.position.y)));
@@ -34,4 +40,24 @@ export function getRandomArbitrary(option: {min: number, max: number}): number {
     const min = Math.ceil(option.min);
     const max = Math.floor(option.max);
     return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+/**
+     * method will calculate correct distance of circle 2 before collision
+     * @param c1 circle 1
+     * @param c2 circle 2, which no calculate correct position
+     * @returns return correct position of c2 before collision
+     */
+export function findCorrectPositionBeforeCollision(c1: Circle, c2: Circle): PointData {
+    const vector = { x: c1.position.x - c2.position.x, y: c1.position.y - c2.position.y };
+    const distance = Math.sqrt(vector.x * vector.x + vector.y * vector.y);
+    const r = c1.radius + c2.radius;
+    const unitVector = { x: vector.x / distance, y: vector.y / distance };
+
+    const correctPosition = {
+        x: c2.position.x - unitVector.x * (r - distance),
+        y: c2.position.y - unitVector.y * (r - distance)
+    };
+
+    return correctPosition;
 }
